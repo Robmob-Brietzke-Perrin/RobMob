b873dd29283ff3bd54a40c155eaa3cb17ddd6397
MrBloue <146952423+MrBloue@users.noreply.github.com>
2026-01-28 16:59:08 +0100

    stuff done

--- src/robmob_cmd_law/src/cmd_law_node.cpp
+++ src/robmob_cmd_law/src/cmd_law_node.cpp
@@ -1,10 +1,37 @@
-#include <cstdio>
+#include "cmd_law_node.hpp"
 
-int main(int argc, char ** argv)
+CmdLawNode::CmdLawNode():
+            Node("cmd_law_node"){compute_path_client_ = rclcpp_action::create_client<ComputePath>(this,"compute_path");}
+
+void CmdLawNode::goal_response_callback(std::shared_future<GoalHandleFibonacci::SharedPtr> future)
 {
-  (void) argc;
-  (void) argv;
-
-  printf("hello world robmob_cmd_law package\n");
-  return 0;
+    auto goal_handle = future.get();
+    if (!goal_handle) {
+    RCLCPP_ERROR(this->get_logger(), "Goal was rejected by server");
+    } else {
+    RCLCPP_INFO(this->get_logger(), "Goal accepted by server, waiting for result");
+    }
 }
+
+void CmdLawNode::feedback_callback(
+    GoalHandleComputePath::SharedPtr,
+    const std::shared_ptr<const ComputePath::Feedback> feedback)
+  {
+    //TODO : ajouter un feedback
+    //Cancel ou non l'action 
+    RCLCPP_INFO(this->get_logger(), ss.str().c_str());
+  }
+
+  void CmdLawNode::result_callback(const GoalHandleComputePath::WrappedResult & result)
+  {
+    if (Result.success)
+        ;
+        //TODO: follow path
+    std::stringstream ss;
+    ss << "Result received: ";
+    for (auto number : result.result->sequence) {
+      ss << number << " ";
+    }
+    RCLCPP_INFO(this->get_logger(), ss.str().c_str());
+    rclcpp::shutdown();
+  }
