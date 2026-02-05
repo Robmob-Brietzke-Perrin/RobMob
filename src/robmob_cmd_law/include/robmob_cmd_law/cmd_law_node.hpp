f1cbc3d9f0ed3e79e735ac1df771de6eaf95e592
MrBloue <146952423+MrBloue@users.noreply.github.com>
2026-02-05 20:49:04 +0100

    action in form need to add functionality

--- src/robmob_cmd_law/include/robmob_cmd_law/cmd_law_node.hpp
+++ src/robmob_cmd_law/include/robmob_cmd_law/cmd_law_node.hpp
@@ -1,69 +1,48 @@
 #ifndef ROBMOB_CMD_LAW__CMD_LAW_NODE_HPP
 #define ROBMOB_CMD_LAW__CMD_LAW_NODE_HPP
-#ifdef ROS_DISTRO_JAZZY
-#include "geometry_msgs/msg/twist_stamped.hpp"
-#endif
-#ifdef ROS_DISTRO_HUMBLE
-#include "geometry_msgs/msg/twist_stamped.hpp"
-#endif
-#include "nav_msgs/msg/path.hpp"
-#include "rclcpp/rclcpp.hpp"
-#include "robmob_interfaces/action/compute_path.hpp"
-#include "rclcpp_action/rclcpp_action.hpp"
-#include <functional>
 
+#include <functional>
+#include <memory>
+#include <string>
+#include <sstream>
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "nav_msgs/msg/path.hpp"
+#include "robmob_interfaces/action/compute_path.hpp"
+
+#ifdef ROS_DISTRO_JAZZY
+  #include "geometry_msgs/msg/twist_stamped.hpp"
+#else
+  #include "geometry_msgs/msg/twist.hpp"
+#endif
 
 class CmdLawNode : public rclcpp::Node
 {
-    public:
-        CmdLawNode();
-        //sub a odom ou un truc comme ca
-        void send_goal()
-        {
-          using namespace std::placeholders;
-            
-          if (!this->client_ptr_->wait_for_action_server()) {
-            RCLCPP_ERROR(this->get_logger(), "Action server not available after waiting");
-            rclcpp::shutdown();
-          }
-      
-          auto goal_msg = ComputePath::Goal();
-      
-          auto send_goal_options = rclcpp_action::Client<ComputePath>::SendGoalOptions();
+public:
+  explicit CmdLawNode(const rclcpp::NodeOptions & options);
+  void send_goal();
 
-          send_goal_options.goal_response_callback =
-            std::bind(&CmdLawNode::goal_response_callback, this, _1);
-
-          send_goal_options.feedback_callback =
-            std::bind(&CmdLawNode::feedback_callback, this, _1, _2);
-
-          send_goal_options.result_callback =
-            std::bind(&CmdLawNode::result_callback, this, _1);
-
-          this->client_ptr_->async_send_goal(goal_msg, send_goal_options);
-        }
-        
-    private:
-        #ifdef ROS_DISTRO_JAZZY
+private:
+    #ifdef ROS_DISTRO_JAZZY
         using TwistCmd = geometry_msgs::msg::TwistStamped;
-        #endif
-        #ifdef ROS_DISTRO_HUMBLE
-            using TwistCmd = geometry_msgs::msg::Twist;
-        #endif  
-        using Path = naw_msgs::msg::Path;
-        using ComputePath = robmob_interfaces::action::ComputePath;
-        using GoalHandleComputePath = rclcpp_action::ClientGoalHandle<ComputePath>;
+    #else
+        using TwistCmd = geometry_msgs::msg::Twist;
+    #endif  
+    
+    using Path = nav_msgs::msg::Path;
+    using ComputePath = robmob_interfaces::action::ComputePath;
+    using GoalHandleComputePath = rclcpp_action::ClientGoalHandle<ComputePath>;
 
-        rclcpp_action::Client<ComputePath>::SharedPtr compute_path_client_;
-        
-        CmdLawNode goal_response_callback(){};
+    // Variables membres
+    rclcpp_action::Client<ComputePath>::SharedPtr client_ptr_;
 
-        void sendcmd_vel();
-    //
+    // Signatures de fonctions (Indispensable pour le std::bind)
+    void goal_response_callback(const GoalHandleComputePath::SharedPtr & goal_handle);
+    void feedback_callback(GoalHandleComputePath::SharedPtr, const std::shared_ptr<const ComputePath::Feedback> feedback);
+    void result_callback(const GoalHandleComputePath::WrappedResult & result);
+    
+    void sendcmd_vel();
 };
 
-
-
-
-
-#endif // ROBMOB_CMD_LAW__CMD_LAW_NODE_HPP
+#endif
