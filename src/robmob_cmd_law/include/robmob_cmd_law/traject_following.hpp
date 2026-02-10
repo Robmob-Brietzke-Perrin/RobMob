3ede3ed223270fce0affe9a4b0c31345c9de8645
MrBloue <146952423+MrBloue@users.noreply.github.com>
2026-02-10 14:01:38 +0100

    modif et ajout suivi de traj

--- src/robmob_cmd_law/include/robmob_cmd_law/traject_following.hpp
+++
@@ -1,7 +0,0 @@
-
-
-class Traj_folowing // uses the RTT action callback to get the path and proceeds to send a contiuous stream of cmd_vels influenced by the dist to it's suposed position
-{
-
-    //calculates the cmd_vel using the dist between it's supposed position and it's current position therefore it need to sub to the turtlebot odom
-};
